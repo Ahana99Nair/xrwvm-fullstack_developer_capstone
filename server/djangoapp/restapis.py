7c332b975571b8572f9e23d3e6319df8a5998a36
Ahana99Nair <ahana.nair22@st.niituniversity.in>
2024-07-22 03:37:14 -0400

    Have made a slight change in restapi.py and did some testing. Posting Reviews not working

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -30,7 +30,7 @@ def analyze_review_sentiments(text):
     request_url = sentiment_analyzer_url+"analyze/"+text
     try:
         response = requests.get(request_url)
-        return response.json
+        return response.json()
     except Exception as err:
         print(f"Unexpected {err=}, {type(err)=}")
         print("Network exception occurred")
@@ -40,7 +40,7 @@ def analyze_review_sentiments(text):
 def post_review(data_dict):
     request_url = backend_url+"/insert_review"
     try:
-        response = request.post(request_url, json=data_dict)
+        response = requests.post(request_url, json=data_dict)
         print(response.json())
         return response.json()
     except:
